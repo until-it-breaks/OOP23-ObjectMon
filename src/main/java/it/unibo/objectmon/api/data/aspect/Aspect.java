7d168c730e89c06d9e83579d4086ea503fea518a
Xu Jiaqi <jiaqi.xu3@studio.unibo.it>
2023-12-13 11:54:23 +0100

    Implemented 'Aspect'

--- src/main/java/it/unibo/objectmon/api/data/aspect/Aspect.java
+++ src/main/java/it/unibo/objectmon/api/data/aspect/Aspect.java
@@ -1,5 +1,60 @@
 package it.unibo.objectmon.api.data.aspect;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *Aspect correspond to Pokémon's Type. Each aspect has an id and a name
+ */
 public enum Aspect {
-    
+
+    /**
+     * Aspect of Normal.
+     */
+    NORMAL(0, "Normal"),
+    /**
+     * Aspect of Grass.
+     */
+    GRASS(1, "Grass"),
+    /**
+     * Aspect of Fire.
+     */
+    FIRE(2, "Fire"),
+    /**
+     * Aspect of Water.
+     */
+    WATER(3, "Water"),
+    /**
+     * Aspect of Flying.
+     */
+    FLYING(4, "Flying"),
+    /**
+     * Aspect of Poison.
+     */
+    POISON(5, "Posion"),
+    /**
+     * Aspect of Ground.
+     */
+    GROUND(0, "Ground"),
+    /**
+     * Aspect of Rock.
+     */
+    ROCK(0, "Rock"),
+    /**
+     * Aspect of Fighting.
+     */
+    FIGHTING(0, "Fighting"),
+    /**
+     * Aspect of Bug.
+     */
+    BUG(0, "Bug");
+
+    private final Map<Integer, String> aspect = new HashMap<>();
+    /*
+    private static final double NEUTRAL_VALUE = 1.0;
+    private static final double SAME_ASPECT_BONUS = 1.5;
+    */
+    Aspect(final int id, final String name) {
+        this.aspect.put(id, name);
+    }
 }
