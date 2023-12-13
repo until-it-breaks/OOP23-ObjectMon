be824ff4c10b5f0040322a1f9eb071ad1bb4c37f
Xu Jiaqi <jiaqi.xu3@studio.unibo.it>
2023-12-13 14:14:23 +0100

    Modified 'SkillCategory' and 'Skill'

--- src/main/java/it/unibo/objectmon/api/data/skill/Skill.java
+++ src/main/java/it/unibo/objectmon/api/data/skill/Skill.java
@@ -1,5 +1,109 @@
 package it.unibo.objectmon.api.data.skill;
 
-public interface Skill {
-    
+/**
+ * The class Skill, that gets used to make:
+ * a Map of the Skill an Objectmon can learn by leveling up;
+ * a Map of Skill of what an Objectmon can use (4 Skills Max).
+ */
+public abstract class Skill {
+    /**
+     *  Name of the Skill.
+     *  Can't be null.
+     */
+    private final String name;
+    /**
+     *  Id of the Skill.
+     *  Can't be negative.
+     */
+    private final int id;
+    /**
+     *  Aspect of the Skill.
+     *  It's used to determine if a Skill is:
+     *  {@see #it.unibo.objectmon.data.skill.SkillStrenght},
+     *  or if a Skill has SameAspectBoost.
+     *  Can't be null
+     */
+    //private final Aspect aspect;
+    /**
+     *  Base power of the Skill.
+     *  Gets added to the offensive stat of the user to determine
+     *  how much damage is inflicted.
+     *  Can't be negative.
+     *  Should only be 0 if it's a Status Skill.
+     */
+    private final int basePower;
+    /**
+     *  Accuracy of the Skill.
+     *  Determines if a Skill will hit or not.
+     * accuracy can have a value between 0 and 101:
+     * 101 If it can't miss;
+     * 
+     * between 0 an 100 a random number is generated.
+     * If the number is higher than the accuracy,
+     * the Skill will fail.
+     */
+    private final int accuracy;
+    /**
+     *  Determines how many times the move can be used it total.
+     *  Can't be 0 or negative.
+     */
+    private final int maxUses;
+    /*
+     *  Determines how many times the move can be used.
+     *  Can be 0, can't go negative.
+     */
+    private final int currentUses;
+    /**
+     *  Category of a skill {@see #it.unibo.objectmon.api.data.skill.SkillCategory}.
+     */
+    //private final SkillCategory category;
+
+    /**
+     * 
+     * @param name
+     * @param id
+     * @param basePower
+     * @param accuracy
+     * @param maxUses
+     * @param currentUses
+     */
+    public Skill(
+        final String name,
+        final int id,
+        final int basePower,
+        final int accuracy,
+        final int maxUses,
+        final int currentUses
+        ) {
+        this.name = name;
+        this.id = id;
+        this.basePower = basePower;
+        this.accuracy = accuracy;
+        this.maxUses = maxUses;
+        this.currentUses = currentUses;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAccuracy() {
+        return accuracy;
+    }
+
+    public int getBasePower() {
+        return basePower;
+    }
+
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentUses() {
+        return currentUses;
+    }
 }
--- src/main/java/it/unibo/objectmon/api/data/skill/SkillCategory.java
+++ src/main/java/it/unibo/objectmon/api/data/skill/SkillCategory.java
@@ -1,36 +1,63 @@
 package it.unibo.objectmon.api.data.skill;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ */
 public enum SkillCategory {
-    PHYS(0,"Physical"),
-    SPEC(1,"Special"),
-    STUS(2,"Status");
-    
-    private final int id;
-    private final String name;
+    /**
+     *Category of a Skill.
+     A Physical Skill calculates damage using the user's Atk
+     against the target's Def.
+     */
+    PHYS(0, "Physical"),
+    /**
+     *Category of a Skill.
+     A Special Skill calculates damage using the user's SpAtk
+     against the target's SpDef.
+     */
+    SPEC(1, "Special"),
+    /**
+     *Category of a Skill.
+     A Status Skill doesn't do damage, but can apply a status to the user or the target.
+     */
+    STUS(2, "Status");
+
+    private final Map<Integer, String> category = new HashMap<>();
 
     /**
-     * 
+     *
      * @param id Id of the DamageType
      * @param name Name of the DamageType
      */
-    private SkillCategory(final int id, final String name){
-        this.id = id;
-        this.name = name;
-    }
-    
-    /**
-     * 
-     * @return Returns the id of the DamageType of the Skill
-     */
-    public int getId() {
-        return id;
+    SkillCategory(final int id, final String name) {
+        category.put(id, name);
     }
 
     /**
      * 
+     * @return Returns the category
+     */
+    public Map<Integer, String> getCategory() {
+        return category;
+    }
+
+    /**
+     *
+     * @return Returns the id of the DamageType of the Skill
+     */
+    public Set<Integer> getId() {
+        return getCategory().keySet();
+    }
+
+    /**
+     * @param key Id of the category
      * @return Returns the name of the DamageType of the Skill
      */
-    public String getName() {
-        return name;
+    public String getName(final int key) {
+        return getCategory().get(key);
     }
 }
