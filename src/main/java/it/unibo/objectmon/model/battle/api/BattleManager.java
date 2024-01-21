5f31dac143648cc00336e750604ddbab58fc3473
Weijie Fu <weijie.fu@studio.unibo.it>
2024-01-21 21:24:24 +0100

    interface BattleManager created

---
+++ src/main/java/it/unibo/objectmon/model/battle/api/BattleManager.java
@@ -0,0 +1,36 @@
+package it.unibo.objectmon.model.battle.api;
+import java.util.Optional;
+/**
+ * A manager of battle which can start a battle between the player and a fighter.
+ * @param <T> is an entity of fighter
+ * @param <O> is an ObjectMon 
+ */
+public interface BattleManager<T, O> {
+    /**
+     * Enum representing the result of a battle.
+     * It can be either WIN or LOSE.
+     */
+    enum Result {
+        /**
+         * Represents the result of winning the battle.
+         */
+        WIN, 
+        /**
+         * Represents the result of losing the battle.
+         */
+        LOSE
+    }
+
+    /**
+     * this method should start battle.
+     * @param player should not be null, if is null the method should throw IllegalArgumentException.
+     * @param enemy it is enemy because if the player meet a wild ObjectMon, the  enemy should be empty
+     * @param objectMon it is wild objectMon, if player meet a fighter, then objectMon should be empty
+     */
+    void startBattle(T player, Optional<T> enemy, Optional<O> objectMon);
+
+    /**
+     * @return the result of the battle
+     */
+    Result getResult();
+}
