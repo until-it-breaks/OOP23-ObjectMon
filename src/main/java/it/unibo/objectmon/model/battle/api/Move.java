29d3217e2ebfad8762a5ee1b11c7ae54f5309290
Weijie Fu <weijie.fu@studio.unibo.it>
2024-01-21 20:18:11 +0100

    interfaces Move and RunAway is created

---
+++ src/main/java/it/unibo/objectmon/model/battle/api/Move.java
@@ -0,0 +1,11 @@
+package it.unibo.objectmon.model.battle.api;
+
+/**
+ * Move represents a possible move in the battle.
+ */
+public interface Move {
+    /**
+     * start the action of move selected.
+     */
+    void action();
+}
