6f9edea53368f276cb3cdfe3948bc43272a3aba3
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2024-01-26 16:11:02 +0100

    inventory interface created

---
+++ src/main/java/it/unibo/objectmon/model/inventory/api/Inventory.java
@@ -0,0 +1,9 @@
+package it.unibo.objectmon.model.inventory.api;
+
+public interface Inventory {
+
+    /**
+     * 
+     */
+    public void setItems();
+}
---
+++ src/main/java/it/unibo/objectmon/model/inventory/impl/InventoryImpl.java
@@ -0,0 +1,19 @@
+package it.unibo.objectmon.model.inventory.impl;
+
+import java.util.ArrayList;
+
+import javax.swing.text.html.parser.Entity;
+
+import it.unibo.objectmon.model.inventory.api.Inventory;
+
+public class InventoryImpl implements Inventory{
+
+    public ArrayList<Entity> inventory = new ArrayList<>();
+
+    @Override
+    public void setItems() {
+        
+    }
+
+
+}
