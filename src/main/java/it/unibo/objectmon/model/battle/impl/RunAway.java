a373cf36480eb503ff8e9b3d1ee555d2594d538a
Weijie Fu <weijie.fu@studio.unibo.it>
2024-01-26 15:26:12 +0100

    modify RunAway considering as a class

---
+++ src/main/java/it/unibo/objectmon/model/battle/impl/RunAway.java
@@ -0,0 +1,15 @@
+package it.unibo.objectmon.model.battle.impl;
+
+import it.unibo.objectmon.model.battle.api.Move;
+
+/**
+ * Player leave the battle.
+ */
+public class RunAway implements Move {
+
+    @Override
+    public void action() {
+        
+    }
+    
+}
