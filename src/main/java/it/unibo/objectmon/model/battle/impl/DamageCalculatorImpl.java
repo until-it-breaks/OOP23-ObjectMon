a8121ee1cbaeb684d71975695775e24da657bc6c
Weijie Fu <weijie.fu@studio.unibo.it>
2024-01-27 01:25:58 +0100

    use pattern strategy to DamageCalculator

--- src/main/java/it/unibo/objectmon/model/battle/impl/DamageCalculatorImpl.java
+++ src/main/java/it/unibo/objectmon/model/battle/impl/DamageCalculatorImpl.java
@@ -1,32 +1,26 @@
 package it.unibo.objectmon.model.battle.impl;
 
-
 import it.unibo.objectmon.api.data.objectmon.Objectmon;
 import it.unibo.objectmon.api.data.skill.PotencyChart;
 import it.unibo.objectmon.api.data.skill.Skill;
 import it.unibo.objectmon.api.data.skill.SkillCategory;
 import it.unibo.objectmon.api.data.statistics.StatId;
-
+import it.unibo.objectmon.model.battle.api.DamageCalculator;
 /**
- * A Calculator that given a skill, it calculate the damage to the target, 
- * considering statics of Objectmon.
+ * a damage calculator calculate the damage in a simple way.
  */
-public class DamageCalculator {
+public class DamageCalculatorImpl implements DamageCalculator {
     private final Skill skill;
     /**
      * Constructor of DamageCalculator.
      * @param skill skill to be used
      */
-    public DamageCalculator(final Skill skill) {
+    public DamageCalculatorImpl(final Skill skill) {
         this.skill = skill;
     }
-    /**
-     * calculate the damage.
-     * @param myObjectmon objectmon that use the skill
-     * @param target objectmon that will be attacked by myObjectmon
-     * @return the damage made to the target
-     */
-    public double damage(final Objectmon myObjectmon, final Objectmon target) {
+
+    @Override
+    public final double damage(final Objectmon myObjectmon, final Objectmon target) {
         final PotencyChart potencyChart = PotencyChart.getChart(this.skill.getAspect().getAspectId());
         final double multiplier = potencyChart.potencyMultiplier(target.getAspect());
         if (skill.getCategory().equals(SkillCategory.PHYS)) {
