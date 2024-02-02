0900ef28932285ed316860c2aa09a2b658b298c9
Weijie Fu <weijie.fu@studio.unibo.it>
2024-02-02 14:58:14 +0100

    implemented RunAway

--- src/main/java/it/unibo/objectmon/model/battle/moves/Impl/RunAway.java
+++ src/main/java/it/unibo/objectmon/model/battle/moves/Impl/RunAway.java
@@ -1,12 +1,23 @@
 package it.unibo.objectmon.model.battle.moves.impl;
 
+import it.unibo.objectmon.model.battle.api.BattleManager;
+import it.unibo.objectmon.model.battle.api.BattleManager.Result;
 import it.unibo.objectmon.model.battle.moves.api.Move;
 
 /**
  * Player leave the battle.
  */
 public class RunAway implements Move {
+    private final BattleManager battleManager;
+    /**
+     * constructor that RunAway has to use battleManager to end the battle.
+     * @param battleManager
+     */
+    public RunAway(final BattleManager battleManager) {
+        this.battleManager = battleManager;
+    }
     @Override
-    public void action() {
+    public final void action() {
+        this.battleManager.setResult(Result.LOSE);
     }
 }
