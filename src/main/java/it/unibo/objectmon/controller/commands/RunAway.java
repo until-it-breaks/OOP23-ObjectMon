6a872498df08d4f93e4f44c1c042d06c7aa96382
Weijie Fu <weijie.fu@studio.unibo.it>
2024-02-05 15:23:55 +0100

    implement class RunAway which implements Command

---
+++ src/main/java/it/unibo/objectmon/controller/commands/RunAway.java
@@ -0,0 +1,15 @@
+package it.unibo.objectmon.controller.commands;
+
+import it.unibo.objectmon.controller.commands.api.Command;
+import it.unibo.objectmon.model.Model;
+/**
+ * Attemps to run away from the battle.
+ */
+public class RunAway implements Command {
+
+    @Override
+    public void execute(final Model model) {
+        model.getGameManager().getBattleManager().runAway();
+    }
+    
+}
