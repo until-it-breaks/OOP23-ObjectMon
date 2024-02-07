7dcde92da5e2c4a74e2b691afd45035d50a8b6c9
Weijie Fu <weijie.fu@studio.unibo.it>
2024-02-07 18:04:09 +0100

    redame Turn to TurnImpl; create an interface Turn and TurnImpl implements Turn

---
+++ src/main/java/it/unibo/objectmon/model/battle/api/Turn.java
@@ -0,0 +1,50 @@
+package it.unibo.objectmon.model.battle.api;
+
+import it.unibo.objectmon.api.data.objectmon.Objectmon;
+import it.unibo.objectmon.model.battle.moves.api.Move;
+
+/**
+ * turn should choose in the battle who has the priority to use an move.
+ */
+public interface Turn {
+    /**
+     * stat turn of.
+     */
+    enum StatTurn {
+        /**
+         * waiting player choose a move.
+         */
+        IS_WAITING_MOVE,
+        /**
+         * AI (trainer or Objectmon) can start to execute his move.
+         */
+        AI_TURN,
+        /**
+         * player can start execute his move.
+         */
+        PLAYER_TURN,
+        /**
+         * when player and AI execute their moves.
+         */
+        TURN_COMPLETE
+    }
+    /**
+     * 
+     * @param moveAI type move of AI
+     * @param movePLayer type move of player
+     * @param playerObjectmon current objectmon of player
+     * @param aiObjectmon current objectmon of ai
+     * @return who has the first turn.
+     */
+    StatTurn whichFirst(Move moveAI, Move movePLayer, Objectmon playerObjectmon, Objectmon aiObjectmon);
+    /**
+     * 
+     * @return get Stat turn.
+     */
+    StatTurn getStat();
+    /**
+     * 
+     * @param statTurn current stat turn.
+     */
+    void setTurn(StatTurn statTurn);
+}
--- src/main/java/it/unibo/objectmon/model/battle/impl/TurnImpl.java
+++ src/main/java/it/unibo/objectmon/model/battle/impl/TurnImpl.java
@@ -2,31 +2,22 @@ package it.unibo.objectmon.model.battle.impl;
 
 import it.unibo.objectmon.api.data.objectmon.Objectmon;
 import it.unibo.objectmon.api.data.statistics.StatId;
+import it.unibo.objectmon.model.battle.api.Turn;
 import it.unibo.objectmon.model.battle.moves.api.Move;
-
-public final class Turn {
+/**
+ * implementation of turn.
+ */
+public final class TurnImpl implements Turn {
     private StatTurn stat;
+
     /**
-     * stat turn of.
+     * constructor.
      */
-    public enum StatTurn {
-        /**
-         * waiting player choose a move.
-         */
-        IS_WAITING_MOVE,
-        /**
-         * AI (trainer or Objectmon) can start to execute his move.
-         */
-        AI_TURN,
-        /**
-         * player can start execute his move.
-         */
-        PLAYER_TURN
-    }
-    public Turn() {
+    public TurnImpl() {
         this.stat = StatTurn.IS_WAITING_MOVE;
     }
 
+    @Override
     public StatTurn whichFirst(
             final Move moveAI, 
             final Move movePLayer,
@@ -34,25 +25,25 @@ public final class Turn {
             final Objectmon aiObjectmon
         ) {
             return moveAI.equals(Move.ATTACK)
-            ? (movePLayer.equals(Move.ATTACK)
+            ? movePLayer.equals(Move.ATTACK)
                 ? whichUseSkillFirst(playerObjectmon, aiObjectmon)
-                : StatTurn.PLAYER_TURN)
-            : (movePLayer.equals(Move.ATTACK)
+                : StatTurn.PLAYER_TURN
+            : movePLayer.equals(Move.ATTACK)
                 ? StatTurn.AI_TURN
-                : StatTurn.PLAYER_TURN);
+                : StatTurn.PLAYER_TURN;
     }
 
-    public StatTurn whichUseSkillFirst(final Objectmon playerObjectmon, final Objectmon aiObjectmon) {
+    private StatTurn whichUseSkillFirst(final Objectmon playerObjectmon, final Objectmon aiObjectmon) {
         return playerObjectmon.getStats().getSingleStat(StatId.SPD) 
             >= aiObjectmon.getStats().getSingleStat(StatId.SPD)
             ? StatTurn.PLAYER_TURN
             : StatTurn.AI_TURN;
     }
-
+    @Override
     public StatTurn getStat() {
         return this.stat;
     }
-
+    @Override
     public void setTurn(final StatTurn statTurn) {
         this.stat = statTurn;
     }
