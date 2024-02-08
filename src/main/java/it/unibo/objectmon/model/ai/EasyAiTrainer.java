e620aaa8c94719fe5677e8802aee595d2df5d6c0
Weijie Fu <weijie.fu@studio.unibo.it>
2024-02-09 00:07:04 +0100

    let ai can switch directly on ObjectmonParty

--- src/main/java/it/unibo/objectmon/model/ai/EasyAiTrainer.java
+++ src/main/java/it/unibo/objectmon/model/ai/EasyAiTrainer.java
@@ -1,15 +1,13 @@
 package it.unibo.objectmon.model.ai;
-import java.util.List;
-
 
 import it.unibo.objectmon.model.ai.api.AiTrainer;
-import it.unibo.objectmon.model.data.api.objectmon.Objectmon;
 import it.unibo.objectmon.model.data.api.objectmon.ObjectmonParty;
 /**
  * an implementation of ai trainer, 
  * which change the objectmon only if the current fighting objectmon is dead.
  */
 public final class EasyAiTrainer extends EasyAiObjectmon implements AiTrainer {
+    private static final int SIZE = 6;
     private int index;
     /**
      * Constructor.
@@ -19,8 +17,9 @@ public final class EasyAiTrainer extends EasyAiObjectmon implements AiTrainer {
     }
 
     @Override
-    public List<Objectmon> switchObjectmon(final ObjectmonParty team) {
-        team.switchPosition(team.getParty().get(0), team.getParty().get(this.index++));
-        return team.getParty();
+    public void switchObjectmon(final ObjectmonParty team) {
+        if (index < EasyAiTrainer.SIZE) {
+            team.switchPosition(team.getParty().get(0), team.getParty().get(this.index++));
+        }
     }
 }
--- src/main/java/it/unibo/objectmon/model/ai/api/AiTrainer.java
+++ src/main/java/it/unibo/objectmon/model/ai/api/AiTrainer.java
@@ -1,10 +1,6 @@
 package it.unibo.objectmon.model.ai.api;
 
-import java.util.List;
-
-import it.unibo.objectmon.model.data.api.objectmon.Objectmon;
 import it.unibo.objectmon.model.data.api.objectmon.ObjectmonParty;
-
 
 /**
  * AI controls trainer in the battle with player.
@@ -12,8 +8,7 @@ import it.unibo.objectmon.model.data.api.objectmon.ObjectmonParty;
 public interface AiTrainer extends AiObjectmon {
     /**
      * switch objectmon when ai think it is necessary.
-     * @param team team in the battle.
-     * @return team with objecmon position changed.
+     * @param team team of the battle
      */
-    List<Objectmon> switchObjectmon(ObjectmonParty team);
+    void switchObjectmon(ObjectmonParty team);
 } 
