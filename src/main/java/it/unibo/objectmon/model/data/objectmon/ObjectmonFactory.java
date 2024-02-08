e548d6085c334bbcc59c5270d3fa083859b757a6
Xu Jiaqi <jiaqi.xu3@studio.unibo.it>
2024-02-08 20:27:27 +0100

    Applied generalization to ObjectmonFactory Also changed the comparison variable from String to ObjectmonEnum.

--- src/main/java/it/unibo/objectmon/model/data/objectmon/ObjectmonFactory.java
+++ src/main/java/it/unibo/objectmon/model/data/objectmon/ObjectmonFactory.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import com.google.gson.Gson;
+
+import it.unibo.objectmon.model.data.api.objectmon.Objectmon;
 /**
  * A factory of Skills.
  */
@@ -13,14 +15,14 @@ public final class ObjectmonFactory {
 
     /**
      * Creates the Objectmons that Trainers are going to use.
-     * @param names Names of the objectmon to be created.
+     * @param objectmons The Objectmons to be created.
      * @param level Level of the Objectmon to be created.
      * @return Returns the set of Objectmon desired.
      */
-    public static Set<ObjectmonImpl> createObjectmonSet(final List<String> names, final int level) {
-        final Set<ObjectmonImpl> objectmonSet = new HashSet<>();
-        for (final String name : names) {
-            objectmonSet.add(createObjectmon(name, level));
+    public static Set<Objectmon> createObjectmonSet(final List<ObjectmonEnum> objectmons, final int level) {
+        final Set<Objectmon> objectmonSet = new HashSet<>();
+        for (final ObjectmonEnum objectmon : objectmons) {
+            objectmonSet.add(createObjectmon(objectmon, level));
         }
         return objectmonSet;
     }
@@ -29,33 +31,33 @@ public final class ObjectmonFactory {
      * Creates a set for the method toJson.
      * @return Returns the complete set of all the Objectmon.
      */
-    private static Set<ObjectmonImpl> createObjectmonSet() {
-        final Set<ObjectmonImpl> objectmonSet = new HashSet<>();
+    private static Set<Objectmon> createObjectmonSet() {
+        final Set<Objectmon> objectmonSet = new HashSet<>();
         final int level = 5;
         for (final var objectmon : ObjectmonEnum.values()) {
-            objectmonSet.add(createObjectmon(objectmon.getName(), level));
+            objectmonSet.add(createObjectmon(objectmon, level));
         }
         return objectmonSet;
     }
 
     /**
-     *
-     * @param name Name of the objectmon to be created.
+     * Creates an Objectmon from ObjectmonEnum and level.
+     * @param objectmon Objectmon to be created.
      * @param level Level of the objectmon to be created.
      * @return The objectmon desired.
      */
-    private static ObjectmonImpl createObjectmon(final String name, final int level) {
+    private static Objectmon createObjectmon(final ObjectmonEnum objectmon, final int level) {
         for (final var objmon : ObjectmonEnum.values()) {
-            if (objmon.getName().equals(name)) {
+            if (objmon == objectmon) {
                 return new ObjectmonImpl.Builder(objmon, level).build();
             }
         }
-        throw new IllegalArgumentException("No such objectmon : " + name);
+        throw new IllegalArgumentException("No such objectmon : " + objectmon);
     }
 
     /**
-     * Converts a set of ObjectmonImpl to a json file.
-     * @return Returns a json of the set of ObjectmonImpl.
+     * Converts a set of Objectmon to a json file.
+     * @return Returns a json of the set of Objectmon.
      */
     public static String toJson() {
         final Gson gson = new Gson();
