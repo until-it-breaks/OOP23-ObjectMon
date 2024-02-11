23151d51ddc7c8907918fa3f779f08b78e752ffd
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2024-02-11 18:06:01 +0100

    added javadocs

--- src/main/java/it/unibo/objectmon/model/item/inventory/Inventories.java
+++ src/main/java/it/unibo/objectmon/model/item/inventory/Inventories.java
@@ -6,13 +6,21 @@ import it.unibo.objectmon.model.item.impl.BallItemImpl;
 import it.unibo.objectmon.model.item.impl.HealItemImpl;
 import it.unibo.objectmon.model.item.inventory.impl.InventoryImpl;
 
+/**
+ * Utility class for creating inventory instances with predefined sets of items.
+ */
 public final class Inventories {
 
     private static final int BASIC_COUNT = 5;
-    
+
     private Inventories() {
     }
 
+    /**
+     * Creates an inventory with basic initial items.
+     * Each basic item (OBJECTBALL/POTION) is added with a count of {@value BASIC_COUNT}.
+     * @return an instance of InventoryImpl with basic initial items
+     */
     public static InventoryImpl createWithBasicInitialItems() {
         final InventoryImpl inventory = new InventoryImpl();
         inventory.addItem(new BallItemImpl(BallEnum.OBJECTBALL), BASIC_COUNT);
@@ -20,6 +28,11 @@ public final class Inventories {
         return inventory;
     }
 
+    /**
+     * Creates an inventory with one of each item.
+     * Each item (Ball/Heal) is added with a count of 1.
+     * @return an instance of InventoryImpl with one of each type of item
+     */
     public static InventoryImpl createWithOneOfEachItem() {
         final InventoryImpl inventory = new InventoryImpl();
         inventory.addItem(new BallItemImpl(BallEnum.OBJECTBALL), 1);
