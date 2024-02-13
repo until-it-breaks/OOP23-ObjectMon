2fad3c7cbde326bba1598a25bd5945f48d0f6f75
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2024-02-13 16:14:30 +0100

    BallEnum update

--- src/main/java/it/unibo/objectmon/model/item/api/BallEnum.java
+++ src/main/java/it/unibo/objectmon/model/item/api/BallEnum.java
@@ -7,28 +7,40 @@ public enum BallEnum {
     /**
      * The most common objectball.
      */
-    OBJECTBALL("Object Ball", 100, 1.0),
+    OBJECTBALL("Object Ball", 250, 1.0, "/items/objectball.png", 
+    "A standard objectball with no special properties."),
     /**
      * An average objectball.
      */
-    MEGABALL("Mega Ball", 200, 1.5),
+    GREATBALL("Great Ball", 500, 1.5, "/items/objectball.png", 
+    "An average objectball, but still slighty superior to the standard one."),
     /**
      * The strongest objectball available commercially.
      */
-    ULTRABALL("Ultra Ball", 400, 2.0),
+    ULTRABALL("Ultra Ball", 1000, 2.0, "/items/ultraball.png", 
+    "The most reliable objectball available out there, has great performance but it's pricey."),
     /**
      * An sure-fire objectball, one of a kind.
      */
-    MASTERBALL("Master Ball", 3000, Double.POSITIVE_INFINITY);
+    MASTERBALL("Master Ball", 3000, Double.POSITIVE_INFINITY, "/items/masterball.png", 
+    "The best of the best, capture is guaranteed but it's prohibitively expensive.");
 
     private final String name;
     private final int value;
     private final double catchRateMultiplier;
+    private final String imagePath;
+    private final String description;
 
-    BallEnum(final String name, final int value, final double catchRateMultiplier) {
+    BallEnum(final String name, 
+        final int value, 
+        final double catchRateMultiplier, 
+        final String imagePath, 
+        final String description) {
         this.name = name;
         this.value = value;
         this.catchRateMultiplier = catchRateMultiplier;
+        this.imagePath = imagePath;
+        this.description = description;
     }
 
     /**
@@ -53,6 +65,22 @@ public enum BallEnum {
      */
     public String getName() {
         return this.name;
+    }
+
+    /**
+     * returns the image path of the objectball.
+     * @return imagePath
+     */
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    /**
+     * returns the description of the objectball.
+     * @return description
+     */
+    public String getDescription() {
+        return description;
     }
 
 }
