5d06892c229b6d69177ea3264c8a057e1e5afc18
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2024-02-14 18:22:52 +0100

    Trade manager with penalty ratio for selling item

---
+++ src/main/java/it/unibo/objectmon/model/item/trademanager/impl/TradeManagerWithPenalty.java
@@ -0,0 +1,41 @@
+package it.unibo.objectmon.model.item.trademanager.impl;
+
+import it.unibo.objectmon.model.entities.api.Player;
+import it.unibo.objectmon.model.item.api.Item;
+import it.unibo.objectmon.model.item.trademanager.api.TradeManager;
+
+public class TradeManagerWithPenalty implements TradeManager {
+
+    private final TradeManager tradeManager;
+    private final double penaltyRatio;
+
+    public TradeManagerWithPenalty(final TradeManager tradeManager, final double penaltyRatio) {
+        this.tradeManager = tradeManager;
+        this.penaltyRatio = penaltyRatio;
+    }
+
+    @Override
+    public void startTrade() {
+        tradeManager.startTrade();
+    }
+
+    @Override
+    public void stopTrade() {
+        tradeManager.stopTrade();
+    }
+
+    @Override
+    public boolean buyItem(Player player, Item item) {
+       return tradeManager.buyItem(player, item);
+    }
+
+    @Override
+    public boolean sellItem(Player player, Item item) {
+        if (tradeManager.sellItem(player, item)) {
+            player.getInventory().withdrawCredits((int) (item.getValue() * penaltyRatio));
+            return true;
+        }
+        return false;
+    }
+    
+}
