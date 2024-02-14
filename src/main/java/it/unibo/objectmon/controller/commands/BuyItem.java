7746d9e0503ef0cd1538554b3fa47fc0d42de3be
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2024-02-14 17:00:30 +0100

    Model implementation updated and buyItem command done

---
+++ src/main/java/it/unibo/objectmon/controller/commands/BuyItem.java
@@ -0,0 +1,24 @@
+package it.unibo.objectmon.controller.commands;
+
+import it.unibo.objectmon.controller.commands.api.Command;
+import it.unibo.objectmon.model.Model;
+import it.unibo.objectmon.model.item.api.Item;
+
+public class BuyItem implements Command {
+
+    private final Item item;
+
+    /**
+     * Construct the item buying.
+     * @param item item
+     */
+    public BuyItem(final Item item) {
+        this.item = item;
+    }
+    
+    @Override
+    public void execute(final Model model) {
+        model.getTradeManager().buyItem(model.getGameContext().getPlayer(), item);
+    }
+    
+}
--- src/main/java/it/unibo/objectmon/model/ModelImpl.java
+++ src/main/java/it/unibo/objectmon/model/ModelImpl.java
@@ -11,6 +11,7 @@ import it.unibo.objectmon.model.encounters.api.RandomEncounterManager;
 import it.unibo.objectmon.model.encounters.impl.RandomEncounterManagerImpl;
 import it.unibo.objectmon.model.gamestate.GameState;
 import it.unibo.objectmon.model.gamestate.GameStateManager;
+import it.unibo.objectmon.model.item.trademanager.api.TradeManager;
 import it.unibo.objectmon.model.misc.battlelog.api.BattleLogger;
 import it.unibo.objectmon.model.misc.collision.api.CollisionManager;
 import it.unibo.objectmon.model.misc.interaction.api.InteractionManager;
@@ -26,27 +27,31 @@ public final class ModelImpl implements Model {
     private final CollisionManager collisionManager;
     private final BattleManager battleManager;
     private final GameStateManager gameStateManager;
+    private final TradeManager tradeManager;
     private final RandomEncounterManager randomEncounterManager;
 
     /**
      * Constructs a ModelImpl instance with the provided dependencies.
      *
-     * @param gameContext               The game context containing information about the game world and entities.
-     * @param interactionManager        The manager responsible for handling interactions within the game.
-     * @param collisionManager          The manager responsible for collision detection.
-     * @param battleManager             The manager responsible for handling battles between entities.
-     * @param gameStateManager          The manager responsible for managing the game state.
-     * @param randomEncounterManager    The manager responsible for triggering random encounters
+     * @param gameContext                   The game context containing information about the game world and entities.
+     * @param interactionManager            The manager responsible for handling interactions within the game.
+     * @param collisionManager              The manager responsible for collision detection.
+     * @param battleManager                 The manager responsible for handling battles between entities.
+     * @param gameStateManager              The manager responsible for managing the game state.
+     * @param tradeManager                  The manager responsible for managing the trade mode.
+     * @param randomEncounterManagerImpl    The manager responsible for triggering random encounters.
      */
     @SuppressFBWarnings(value = "EI2", justification = "Allowing mutable objects to be stored for flexibility")
     public ModelImpl(final GameContext gameContext, final InteractionManager interactionManager,
             final CollisionManager collisionManager, final BattleManager battleManager,
-            final GameStateManager gameStateManager, final RandomEncounterManager randomEncounterManagerImpl) {
+            final GameStateManager gameStateManager, final TradeManager tradeManager, 
+            final RandomEncounterManager randomEncounterManagerImpl) {
         this.gameContext = gameContext;
         this.interactionManager = interactionManager;
         this.collisionManager = collisionManager;
         this.battleManager = battleManager;
         this.gameStateManager = gameStateManager;
+        this.tradeManager = tradeManager;
         this.randomEncounterManager = randomEncounterManagerImpl;
     }
 
@@ -63,6 +68,11 @@ public final class ModelImpl implements Model {
     @Override
     public Optional<Battle> getBattleStats() {
         return this.battleManager.getBattleStats();
+    }
+
+    @Override
+    public TradeManager getTradeManager() {
+        return this.tradeManager;
     }
 
     @Override
