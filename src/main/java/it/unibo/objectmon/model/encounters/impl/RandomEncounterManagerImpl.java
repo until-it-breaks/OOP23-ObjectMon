547ab6b34a2f659eeba3b8f6dcfe367e079168b7
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-02-14 23:48:34 +0100

    Fixed deserialization issue, again

--- src/main/java/it/unibo/objectmon/model/encounters/impl/RandomEncounterManagerImpl.java
+++ src/main/java/it/unibo/objectmon/model/encounters/impl/RandomEncounterManagerImpl.java
@@ -42,7 +42,7 @@ public final class RandomEncounterManagerImpl implements Observer, RandomEncount
     @Override
     public void performCheck() {
         final Coord position = gameContext.getPlayer().getPosition();
-        if (gameContext.getWorld().getMap().get(position).isTriggerEncounter() && encountersOccurs()) {
+        if (gameContext.getWorld().getMap().get(position).isTriggersEncounters() && encountersOccurs()) {
             startRandomEncounter();
         }
     }
