ae2231c43c36f258c6efe55eeca5d69c75f037d3
Garcia Rufer Azael <azael.garciarufer@studio.unibo.it>
2024-02-14 13:55:33 +0100

    Added TRADE mode

---
+++ src/main/java/it/unibo/objectmon/model/item/trademanager/impl/TradeManagerImpl.java
@@ -0,0 +1,33 @@
+package it.unibo.objectmon.model.item.trademanager.impl;
+
+import it.unibo.objectmon.model.entities.api.Player;
+import it.unibo.objectmon.model.item.api.Item;
+import it.unibo.objectmon.model.item.trademanager.api.TradeManager;
+
+public class TradeManagerImpl implements TradeManager {
+
+    @Override
+    public void startTrade() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startTrade'");
+    }
+
+    @Override
+    public void stopTrade() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stopTrade'");
+    }
+
+    @Override
+    public boolean buyItem(Player player, Item item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buyItem'");
+    }
+
+    @Override
+    public boolean sellItem(Player player, Item item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sellItem'");
+    }
+    
+}
